ff8dc29c09ed9f685e34c732a1480477b4ac54f1
Brandon Fonnemann <branjosef@gmail.com>
2020-08-27 11:59:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,31 @@
 # require modules here
+require "yaml"
+require "pry"
 
-def load_library
-  # code goes here
+def load_library(file)
+# code goes here
+library = YAML.load_file(file)
+test = library
+  test.each do |key, value|
+    test[key] = {:english => test.dig(key, 0), :japanese => test.dig(key,1)}
+  end
+test
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_english_meaning(file, emoticon)
+test = load_library(file)
+  test.each do |k, v|
+   if test[k][:japanese] == emoticon
+    return k
+   end
+  end
 end
 
-def get_english_meaning
-  # code goes here
+def get_japanese_emoticon(file, emoticon)
+test = load_library(file)
+  test.each do |k, v|
+   if test[k][:english] == emoticon
+    return test[k][:japanese]
+   end
+  end
 end
